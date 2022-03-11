c35be058dc21952ba5d888e3cbb2c61d01d4b749
Newt-JH <flozzang@naver.com>
2022-03-11 21:10:02 +0900

    Initial commit

---
+++ src/main/java/com/example/newrt/NewrtApplication.java
@@ -0,0 +1,13 @@
+package com.example.newrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewrtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewrtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newrt/NewrtApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.newrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewrtApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("context Loads ...");
+    }
+
+}
